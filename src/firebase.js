1ee54bc79d73434b32115b847a97b2cb89c4702c
JongGyuChoi <chlchl5745@gmail.com>
2021-02-27 13:00:23 +0900

    add package

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCmQtfoGeTj60VLROSPhAFMx3EnWbvA8q0",
+  authDomain: "slack-clone-f9b33.firebaseapp.com",
+  projectId: "slack-clone-f9b33",
+  storageBucket: "slack-clone-f9b33.appspot.com",
+  messagingSenderId: "37155094766",
+  appId: "1:37155094766:web:241d86608bc2ca1529f3ff"
+};
